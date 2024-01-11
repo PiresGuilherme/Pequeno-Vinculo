67ae1d82f324835a0a32266adc26d5875e3c88bd
PiresGuilherme <vianaguilhermepv@gmail.com>
2024-01-10 23:33:40 -0300

    criand tela de avaliação de alunos de uma turma.

---
+++ frontend/src/pages/teacher/performance.ts
@@ -0,0 +1,11 @@
+//@ts-ignore
+import axios from "https://cdn.jsdelivr.net/npm/axios@1.3.5/+esm";
+var classe = 1
+
+async function evaluateClass(){
+ try {
+    const response = await axios.
+ } catch (error) {
+    
+ }
+}
