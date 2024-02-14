cf70ae81d1955c2e1a88659843cb18165a30ddf9
PiresGuilherme <vianaguilhermepv@gmail.com>
2024-02-14 18:38:59 -0300

    tentativa de fazer a rota para AWS

--- backend/src/routes/classRoutes.ts
+++ backend/src/routes/classRoutes.ts
@@ -2,17 +2,40 @@ import { Request, Response, Router } from "express";
 import { ClassController } from "../controllers/ClassController";
 import { FileController, reqFile } from "../controllers/FileController";
 
-const multer  = require('multer')
+var AWS = require('aws-sdk');
+AWS.config.update({
+  accessKeyId: 'AKIATCKAR55ZBOCTGA7A',
+  secretAccessKey: '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',
+  region: 'sa-east-1',
+});
+
+// const { S3Client } = require('@aws-sdk/client-s3')
+const multerS3 = require('multer-s3')
+const multer = require('multer')
+const s3 = new AWS.S3();
+
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, 'uploads')
-    },
-    filename: function (req, file, cb) {
-      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-      cb(null, file.fieldname + '-' + uniqueSuffix +"-" + file.originalname);
-    }
-  })
-const upload = multer({storage})
+    storage: multerS3({
+        s3: s3,
+        bucket: 'pequenovinculo',
+        metadata: function (req, file, cb) {
+            cb(null, Object.assign({}, req.body));
+          },
+
+        key: function (req, file, cb) {
+            const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+            cb(null, file.fieldname + '-' + uniqueSuffix + "-" + file.originalname)
+        }
+    })
+    // destination: function (req, file, cb) {
+    //   cb(null, 'uploads')
+    // },
+    // filename: function (req, file, cb) {
+    //   const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    //   cb(null, file.fieldname + '-' + uniqueSuffix +"-" + file.originalname);
+    // }
+})
+const upload = multer({ storage })
 const router = Router()
 // let classController = new ClassController()
 
@@ -36,15 +59,17 @@ router.get('/class/id', (req: Request, res: Response) => {
     classController.findOneClass(req, res)
 })
 
-router.get("/class/:id(\\d+)/picture", upload.single('picture'), (req:Request,res:Response)=>{
+router.get("/class/:id(\\d+)/picture", upload.single('picture'), (req: Request, res: Response) => {
     const fileController = new FileController();
     console.log("1");
-    fileController.getClassFiles(req,res);
+    fileController.getClassFiles(req, res);
 })
-router.post("/class/:id(\\d+)/picture",upload.single('picture'), async (req:reqFile,res:Response)=>{
+router.post("/class/:id(\\d+)/picture", upload.single('picture'), async (req: reqFile, res: Response) => {
+    console.log(req.file);
+    
     const fileController = new FileController();
     console.log("1");
-    fileController.newPicture(req,res);
+    fileController.newPicture(req, res);
 })
 
 export { router };
