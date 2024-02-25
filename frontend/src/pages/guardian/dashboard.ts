98ef1a0fdd212c18096d106b612dfd41164f180c
GabrielMorez <gabrielmorez@hotmail.com>
2024-02-25 18:38:58 -0300

    Finalizado Calendário

--- frontend/src/pages/guardian/dashboard.ts
+++ frontend/src/pages/guardian/dashboard.ts
@@ -1,11 +1,11 @@
 const token = localStorage.getItem('login');
 if (!token) {
-  window.location.href = "/frontend/src/pages/initial-login.html";
+    window.location.href = "/frontend/src/pages/initial-login.html";
 }
 
 //@ts-ignore
 import axios from "https://cdn.jsdelivr.net/npm/axios@1.3.5/+esm";
-
+calendar();
 const backend = "http://localhost:3000/api"
 
 const userJson = localStorage.getItem('login');
@@ -41,7 +41,7 @@ async function findChildren(userId: number) {
                 colorIndex = (colorIndex + 1) % colorPalette.length;
                 children.appendChild(divChildren);
             });
-        } 
+        }
     } catch (error: any) {
         console.error('sa:', error.message);
     }
@@ -82,7 +82,7 @@ async function childrensPerformance(userId: number) {
                 bestChildrens.appendChild(divBests);
 
             });
-        } 
+        }
     } catch (error: any) {
         console.error('sa:', error.message);
     }
@@ -161,7 +161,56 @@ document.addEventListener("DOMContentLoaded", () => {
     logoutButton.addEventListener('click', logout)
 });
 
-$(document).ready(function() {
+async function calendar() {
+    type Day = {
+        "day": number | string,
+        "weekDay": number
+    }
+    const elementMonth = document.getElementById('month')!;
+    const month = new Date().toLocaleString('default', { month: 'long' })
+    const daysOfMonth = new Date(new Date().getFullYear(), new Date().getMonth() + 1, 0).getDate()
+    const weeksOfMonth: Day[] = []
+
+    elementMonth.innerHTML = `${month.toUpperCase()}`
+    for (let i = 1; i <= daysOfMonth; i++) {
+        let day: Day = {
+            "day": new Date(new Date().getFullYear(), new Date().getMonth(), i).getDate(),
+            "weekDay": new Date(new Date().getFullYear(), new Date().getMonth(), i).getDay()
+        }
+        weeksOfMonth.push(day)
+    }
+
+    if (weeksOfMonth[0].weekDay != 0) {
+        let indexOfDay = weeksOfMonth[0].weekDay
+
+        for (let i = indexOfDay - 1; i >= 0; i--) {
+            weeksOfMonth.splice(0, 0, { "day": '', "weekDay": i })
+        }
+    }
+
+    if (weeksOfMonth[weeksOfMonth.length - 1].weekDay != 6) {
+        let indexOfDay = weeksOfMonth[weeksOfMonth.length - 1].weekDay;
+
+        for (let i = indexOfDay + 1; i <= 6; i++) {
+            weeksOfMonth.push({ "day": '', "weekDay": i })
+        }
+    }
+
+    for (let i = 0, count = 0; i <= 5; i++) {
+        for (let index = 0; index <= 6 && count <= weeksOfMonth.length - 1; index++, count++) {
+            let dayElement = document.getElementById(`day-${i}/${index}`)!
+            dayElement.innerHTML = `${weeksOfMonth[count].day}`
+            if (weeksOfMonth[count].day === new Date().getDate()) {
+                dayElement.classList.add('today')
+            }
+            if (Number(weeksOfMonth[count].day) < new Date().getDate()) {
+                dayElement.classList.add('scnd-font-color')
+            }
+        }
+    }
+}
+
+$(document).ready(function () {
     $("#active-home").addClass("active");
     $("#active-schedule").removeClass('active');
     $("#active-gallery").removeClass("active")
